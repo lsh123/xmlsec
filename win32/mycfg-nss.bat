e925cb69f213eb486932d15682dff27cd029a91a
Aleksey Sanin <aleksey@src.gnome.org>
2003-03-03 22:56:40 +0000

    changed my scripts for windows

--- win32/mycfg-nss.bat
+++ win32/mycfg-nss.bat
@@ -11,11 +11,11 @@ REM
 SET ICONV=d:\sdk\bin\iconv
 SET LIBXML2=d:\sdk\bin\libxml2
 SET LIBXSLT=d:\sdk\bin\libxslt
-SET OPENSSL=d:\sdk\bin\openssl
+SET NSS=d:\sdk\bin\mozilla
 SET XMLSEC=d:\sdk\bin\xmlsec
-SET XMLSEC_INCLUDE=%ICONV%\include;%LIBXML2%\include;%LIBXSLT%\include;%OPENSSL%\include
-SET XMLSEC_LIB=%ICONV%\lib;%LIBXML2%\lib;%LIBXSLT%\lib;%OPENSSL%\lib
-SET XMLSEC_OPTIONS=static=yes debug=yes xslt=yes crypto=openssl
+SET XMLSEC_INCLUDE=%ICONV%\include;%LIBXML2%\include;%LIBXSLT%\include;%NSS%\include;%NSS%\include\nspr;%NSS%\public;%NSS%\public\nss
+SET XMLSEC_LIB=%ICONV%\lib;%LIBXML2%\lib;%LIBXSLT%\lib;%NSS%\lib
+SET XMLSEC_OPTIONS=static=yes debug=yes xslt=yes crypto=nss
 
 del /F Makefile configure.txt
 cscript configure.js prefix=%XMLSEC% %XMLSEC_OPTIONS% include=%XMLSEC_INCLUDE% lib=%XMLSEC_LIB% 

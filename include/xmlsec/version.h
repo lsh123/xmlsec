79517f6cc962ac0ad1dec89e1b30bf5edcd8d146
Aleksey Sanin <aleksey@src.gnome.org>
2003-03-20 07:30:09 +0000

    0.0.14 release

--- include/xmlsec/version.h
+++ include/xmlsec/version.h
@@ -20,7 +20,7 @@ extern "C" {
  * The library version string in the format
  * "<major-number>.<minor-number>.<sub-minor-number>".
  */
-#define XMLSEC_VERSION			"0.0.13"
+#define XMLSEC_VERSION			"0.0.14"
 /**
  * XMLSEC_VERSION_MAJOR:
  *
@@ -38,7 +38,7 @@ extern "C" {
  *
  * The library sub-minor version number.
  */
-#define XMLSEC_VERSION_SUBMINOR		13
+#define XMLSEC_VERSION_SUBMINOR		14
 
 /**
  * XMLSEC_VERSION_INFO:
@@ -46,7 +46,7 @@ extern "C" {
  * The library version info string in the format
  * "<major-number>+<minor-number>:<sub-minor-number>:<minor-number>".
  */
-#define XMLSEC_VERSION_INFO		"0:13:0"
+#define XMLSEC_VERSION_INFO		"0:14:0"
 
 
 #ifdef __cplusplus
--- xmlsec.spec
+++ xmlsec.spec
@@ -1,6 +1,6 @@
 Summary: Library providing support for "XML Signature" and "XML Encryption" standards
 Name: xmlsec
-Version: 0.0.13
+Version: 0.0.14
 Release: 1
 License: MIT
 Group: Development/Libraries

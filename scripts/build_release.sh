a12aaa3b71fd9d432869d2b72b5b97ff555ea814
Aleksey Sanin <aleksey@src.gnome.org>
2003-04-03 04:10:11 +0000

    fixed rpm

--- scripts/build_release.sh
+++ scripts/build_release.sh
@@ -1,31 +1,29 @@
 #!/bin/sh 
 
-module=$1
-version=$2
-branch=$3
+version=$1
+branch=$2
 build_root=/tmp
 rpm_root=/usr/src/redhat
-remote_root=ftp.aleksey.com:/var/ftp/pub/$module/releases
+remote_root=ftp.aleksey.com:/var/ftp/pub/xmlsec/releases
 
-rm -rf $build_root/$module
+rm -rf $build_root/xmlsec
 cd $build_root
 
 if test "z$branch" != "z"; then 
-    echo "Cheking out module $module from branch $branch"
-    cvs -z3 co -P -r $branch $module
+    echo "Cheking out module xmlsec from branch $branch"
+    cvs -d :pserver:aleksey@cvs.gnome.org:/cvs/gnome -z3 co -P -r $branch xmlsec
 else
-    echo "Cheking out module $module from tip"
-    cvs -z3 co -P $module
+    echo "Cheking out module xmlsec from tip"
+    cvs -d :pserver:aleksey@cvs.gnome.org:/cvs/gnome -z3 co -P xmlsec
 fi
 
-cd $module
+cd xmlsec
 version_cvs=`echo $version | sed 's/\./_/g'`
-cvs tag -F $module-$version_cvs
+cvs tag -F xmlsec-$version_cvs
 rm -rf `find . -name "CVS"`
-
 ./autogen.sh --prefix=/usr --sysconfdir=/etc
-rm config.cache
+rm -rf config.cache
 make dist
-mv $module-$version.tar.gz $rpm_root/SOURCES
-rpm -ba $module.spec
+mv xmlsec*-$version.tar.gz $rpm_root/SOURCES
+rpmbuild -ba xmlsec1.spec
 
--- scripts/change-release.sh
+++ scripts/change-release.sh
@@ -4,13 +4,14 @@
 old_release=$1
 new_release=$2
 
+sh_files=`find . -name "*.sh" -print`
 am_files=`find . -name "*.am" -print`
 in_files=`find . -name "*.in" -print`
 html_files=`find . -name "*.html" -print`
 sgml_files=`find . -name "*.sgml" -print`
 cvsignore_files=`find . -name ".cvsignore" -print`
 
-files="$am_files $in_files $html_files $sgml_files $cvsignore_files"
+files="$sh_files $am_files $in_files $html_files $sgml_files $cvsignore_files"
 for i in  $files; do
     echo Processing $i ...
     sed "s/$old_release/$new_release/g" $i > $i.tmp

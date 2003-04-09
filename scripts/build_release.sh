ff879d0d5ca3f267020ef08d0f1002972302f234
Aleksey Sanin <aleksey@src.gnome.org>
2003-04-09 05:23:55 +0000

    fixed build script

--- scripts/build_release.sh
+++ scripts/build_release.sh
@@ -1,28 +1,35 @@
 #!/bin/sh 
 
+cur_pwd=`pwd`
+today=`date`
+module=xmlsec
 branch=
-
-build_root=/tmp
+cvs_root=:pserver:aleksey@cvs.gnome.org:/cvs/gnome
 rpm_root=/usr/src/redhat
-remote_root=ftp.aleksey.com:/var/ftp/pub/xmlsec/releases
+build_root="$rpm_root/BUILD/xmlsec-build-area-$today"
+remote_root=aleksey@ftp.aleksey.com:/var/ftp/pub/xmlsec/releases
 
-rm -rf $build_root/xmlsec
-cd $build_root
+echo "Creating build area $build_root"
+rm -rf "$build_root"
+mkdir -p "$build_root"
+cd "$build_root"
 
 if test "z$branch" != "z"; then 
-    echo "Cheking out module xmlsec from branch $branch"
-    cvs -d :pserver:aleksey@cvs.gnome.org:/cvs/gnome -z3 co -P -r $branch xmlsec
+    echo "Cheking out module $module from branch $branch"
+    cvs -d $cvs_root -z3 co -P -r $branch $module > /dev/null
 else
-    echo "Cheking out module xmlsec from tip"
-    cvs -d :pserver:aleksey@cvs.gnome.org:/cvs/gnome -z3 co -P xmlsec
+    echo "Cheking out module $module from tip"
+    cvs -d $cvs_root -z3 co -P $module > /dev/null
 fi
 
 cd xmlsec
-version_cvs=`echo $version | sed 's/\./_/g'`
-cvs tag -F xmlsec-$version_cvs
 ./autogen.sh --prefix=/usr --sysconfdir=/etc
-rm -rf config.cache
-make distclean
-make rpm
-mv xmlsec*.tar.gz $rpm_root/SOURCES
+make rpm-release
 
+tar_file=`ls xmlsec*.tar.gz`
+echo "Moving sources tar file to $rpm_root/SOURCES/$tar_file"
+mv $tar_file $rpm_root/SOURCES
+
+echo "Cleanup"
+cd "$cur_pwd"
+rm -rf "$build_root"

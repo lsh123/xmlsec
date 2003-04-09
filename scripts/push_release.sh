90cc4b97cfd7134fb249c918c1a83b6cb48365d6
Aleksey Sanin <aleksey@src.gnome.org>
2003-04-09 05:39:18 +0000

    push script

--- scripts/push_release.sh
+++ scripts/push_release.sh
@@ -1,7 +1,7 @@
 #!/bin/sh 
 
-module=$1
-version=$2
+version=$1
+module=xmlsec1
 build_root=/tmp
 rpm_root=/usr/src/redhat
 remote_root=aleksey@ftp.aleksey.com:/var/ftp/pub/$module/releases
@@ -12,10 +12,14 @@ scp $rpm_root/SOURCES/$module-$version.tar.gz \
     $rpm_root/SRPMS/$module-$version-*.src.rpm \
     $rpm_root/RPMS/$build_target/$module-$version-*.$build_target.rpm \
     $rpm_root/RPMS/$build_target/$module-devel-$version-*.$build_target.rpm \
+    $rpm_root/RPMS/$build_target/$module-nss-$version-*.$build_target.rpm \
+    $rpm_root/RPMS/$build_target/$module-nss-devel-$version-*.$build_target.rpm \
     $remote_root
 
 echo "Uploading to redhat.com"
 ncftpput incoming.redhat.com /libc6 \
     $rpm_root/SRPMS/$module-$version-*.src.rpm \
     $rpm_root/RPMS/$build_target/$module-$version-*.$build_target.rpm \
-    $rpm_root/RPMS/$build_target/$module-devel-$version-*.$build_target.rpm
+    $rpm_root/RPMS/$build_target/$module-devel-$version-*.$build_target.rpm \
+    $rpm_root/RPMS/$build_target/$module-nss-$version-*.$build_target.rpm \
+    $rpm_root/RPMS/$build_target/$module-nss-devel-$version-*.$build_target.rpm

313c9d7092b3ece0ddd03b2ec83fb7fffa7750d4
Aleksey Sanin <aleksey@src.gnome.org>
2003-05-26 05:06:15 +0000

    made minor documentation and configuration fixes reported by John Belmonte

--- apps/xmlsec.c
+++ apps/xmlsec.c
@@ -52,7 +52,7 @@ static const char bugs[] =
 static const char helpCommands1[] =     
     "Usage: xmlsec <command> [<options>] [<file>]\n"
     "\n"
-    "xmlsec is a command line tool for singining, verifying, encrypting and\n"
+    "xmlsec is a command line tool for signing, verifying, encrypting and\n"
     "decrypting XML documents. The allowed <command> values are:\n"
     "  --help      "	"\tdisplay this help information and exit\n"
     "  --help-all  "	"\tdisplay help information for all commands/options and exit\n"

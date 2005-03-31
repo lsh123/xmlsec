7643e7f2f88c810c1e8e95a2ecb1a613cfaf5df4
Aleksey Sanin <aleksey@src.gnome.org>
2005-03-31 02:28:04 +0000

    new 1.2.8 release preparation

--- include/xmlsec/version.h
+++ include/xmlsec/version.h
@@ -21,7 +21,7 @@ extern "C" {
  * The library version string in the format
  * "<major-number>.<minor-number>.<sub-minor-number>".
  */
-#define XMLSEC_VERSION			"1.2.7"
+#define XMLSEC_VERSION			"1.2.8"
 
 /**
  * XMLSEC_VERSION_MAJOR:
@@ -42,7 +42,7 @@ extern "C" {
  *
  * The library sub-minor version number.
  */
-#define XMLSEC_VERSION_SUBMINOR		7
+#define XMLSEC_VERSION_SUBMINOR		8
 
 /**
  * XMLSEC_VERSION_INFO:
@@ -50,7 +50,7 @@ extern "C" {
  * The library version info string in the format
  * "<major-number>+<minor-number>:<sub-minor-number>:<minor-number>".
  */
-#define XMLSEC_VERSION_INFO		"3:7:2"
+#define XMLSEC_VERSION_INFO		"3:8:2"
 
 
 #ifdef __cplusplus

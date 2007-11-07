6ecf814fbe68ff3b0f19659c806de3bf3555a38a
Aleksey Sanin <aleksey@src.gnome.org>
2007-11-07 06:57:15 +0000

    release script tweaks
    svn path=/trunk/; revision=993

--- scripts/build_release.sh
+++ scripts/build_release.sh
@@ -1,22 +1,22 @@
 #!/bin/sh 
 
-cur_pwd=`pwd`
-today=`date +%F-%T`
-module=xmlsec
-branch=trunk
-svn_uri=svn+ssh://aleksey@svn.gnome.org/svn/$module/$branch
+# config
+svn_module=xmlsec
+svn_uri=svn+ssh://aleksey@svn.gnome.org/svn/$svn_module/trunk
 rpm_root=/usr/src/redhat
 build_root="$rpm_root/BUILD/xmlsec-build-area-$today"
-remote_root=aleksey@ftp.aleksey.com:/var/ftp/pub/xmlsec/releases
+
+cur_pwd=`pwd`
+today=`date +%F-%T`
 
 echo "Creating build area $build_root"
 rm -rf "$build_root"
 mkdir -p "$build_root"
 cd "$build_root"
 
-echo "Checking out the module $module"
-svn checkout $svn_uri $module
-cd xmlsec
+echo "Checking out the module $svn_module"
+svn checkout $svn_uri $svn_module
+cd $svn_module
 find . -name ".svn" | xargs rm -r
 
 ./autogen.sh --prefix=/usr --sysconfdir=/etc

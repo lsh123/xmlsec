85d22b9f23680b808d33b1f904312a82e5bfd3e9
Aleksey Sanin <aleksey@aleksey.com>
2016-11-27 08:51:21 -0800

    fix openssl 098 support and rename wrapper file

--- src/openssl/x509.c
+++ src/openssl/x509.c
@@ -39,7 +39,7 @@
 #include <xmlsec/openssl/crypto.h>
 #include <xmlsec/openssl/evp.h>
 #include <xmlsec/openssl/x509.h>
-#include "openssl11_wrapper.h"
+#include "openssl_compat.h"
 
 /*************************************************************************
  *

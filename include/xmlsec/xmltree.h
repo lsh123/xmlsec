c95903a711952b7f63e6e6504da9463097ee6d31
Aleksey Sanin <aleksey@aleksey.com>
2017-04-21 12:07:19 -0700

    remove xmlSecGenerateAndAddID and xmlSecGenerateID

--- include/xmlsec/xmltree.h
+++ include/xmlsec/xmltree.h
@@ -88,13 +88,6 @@ XMLSEC_EXPORT int               xmlSecNodeEncodeAndSetContent
 XMLSEC_EXPORT void              xmlSecAddIDs            (xmlDocPtr doc,
                                                          xmlNodePtr cur,
                                                          const xmlChar** ids);
-XMLSEC_DEPRECATED XMLSEC_EXPORT int xmlSecGenerateAndAddID (xmlNodePtr node,
-                                                         const xmlChar* attrName,
-                                                         const xmlChar* prefix,
-                                                         xmlSecSize len);
-XMLSEC_DEPRECATED XMLSEC_EXPORT xmlChar* xmlSecGenerateID (const xmlChar* prefix,
-                                                         xmlSecSize len);
-
 XMLSEC_EXPORT xmlDocPtr         xmlSecCreateTree        (const xmlChar* rootNodeName,
                                                          const xmlChar* rootNodeNs);
 XMLSEC_EXPORT int               xmlSecIsEmptyNode       (xmlNodePtr node);

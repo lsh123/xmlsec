d555edbd1ff37726525357471c3f3264298f9f11
Aleksey Sanin <aleksey@aleksey.com>
2017-06-13 11:09:06 -0700

    fix examples too

--- tests/testDSig.sh
+++ tests/testDSig.sh
@@ -7,9 +7,11 @@
 ##########################################################################
 ##########################################################################
 echo "--- testDSig started for xmlsec-$crypto library ($timestamp)"
+echo "--- LD_LIBRARY_PATH=$LD_LIBRARY_PATH"
 echo "--- LTDL_LIBRARY_PATH=$LTDL_LIBRARY_PATH"
 echo "--- log file is $logfile"
 echo "--- testDSig started for xmlsec-$crypto library ($timestamp)" >> $logfile
+echo "--- LD_LIBRARY_PATH=$LD_LIBRARY_PATH" >> $logfile
 echo "--- LTDL_LIBRARY_PATH=$LTDL_LIBRARY_PATH" >> $logfile
 
 ##########################################################################

9df755458a3a74fb80d3c744e55213c17a12ea45
Miklos Vajna <vmiklos@collabora.co.uk>
2018-02-09 20:59:11 +0100

    mscng: implement x509 key data duplication

--- src/mscng/x509.c
+++ src/mscng/x509.c
@@ -66,12 +66,52 @@ xmlSecMSCngKeyDataX509Initialize(xmlSecKeyDataPtr data) {
 
 static int
 xmlSecMSCngKeyDataX509Duplicate(xmlSecKeyDataPtr dst, xmlSecKeyDataPtr src) {
+    PCCERT_CONTEXT srcCert = NULL;
+    PCCERT_CONTEXT dstCert;
+    xmlSecMSCngX509DataCtxPtr srcCtx;
+    int ret;
+
     xmlSecAssert2(xmlSecKeyDataCheckId(dst, xmlSecMSCngKeyDataX509Id), -1);
     xmlSecAssert2(xmlSecKeyDataCheckId(src, xmlSecMSCngKeyDataX509Id), -1);
+    srcCtx = xmlSecMSCngX509DataGetCtx(src);
 
-    xmlSecNotImplementedError(NULL);
+    /* duplicate the certificate store */
+    while((srcCert = CertEnumCertificatesInStore(srcCtx->hMemStore, srcCert)) != NULL) {
+        dstCert = CertDuplicateCertificateContext(srcCert);
+        if(dstCert == NULL) {
+            xmlSecMSCngLastError("CertDuplicateCertificateContext",
+                xmlSecKeyDataGetName(dst));
+            return(-1);
+        }
 
-    return(-1);
+        ret = xmlSecMSCngKeyDataX509AdoptCert(dst, dstCert);
+        if(ret < 0) {
+            xmlSecInternalError("xmlSecMSCngKeyDataX509AdoptCert",
+                xmlSecKeyDataGetName(dst));
+            CertFreeCertificateContext(dstCert);
+            return(-1);
+        }
+    }
+
+    if(srcCtx->cert != NULL) {
+        /* have a key certificate, duplicate that */
+        dstCert = CertDuplicateCertificateContext(srcCtx->cert);
+        if(dstCert == NULL) {
+            xmlSecMSCngLastError("CertDuplicateCertificateContext",
+                xmlSecKeyDataGetName(dst));
+            return(-1);
+        }
+
+        ret = xmlSecMSCngKeyDataX509AdoptKeyCert(dst, dstCert);
+        if(ret < 0) {
+            xmlSecInternalError("xmlSecMSCngKeyDataX509AdoptKeyCert",
+                xmlSecKeyDataGetName(dst));
+            CertFreeCertificateContext(dstCert);
+            return(-1);
+        }
+    }
+
+    return(0);
 }
 
 static void

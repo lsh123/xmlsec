a0e429e7bd546843202d40eebc5cda9d5442bdc6
Charlie Li <git@vishwin.info>
2018-03-26 02:10:07 -0400

    LibreSSL 2.7
    LibreSSL 2.7 introduced compatibility with most OpenSSL 1.1 APIs.
    This corrects detection of LibreSSL versions, as well as include
    one missing OpenSSL 1.1 function from LibreSSL 2.7.
    
    openssl_compat.h obtained from OpenBSD Ports collection [1]
    
    [1] https://github.com/openbsd/ports/blob/master/security/xmlsec/patches/patch-src_openssl_openssl_compat_h

--- include/xmlsec/openssl/crypto.h
+++ include/xmlsec/openssl/crypto.h
@@ -43,12 +43,12 @@ XMLSEC_CRYPTO_EXPORT const xmlChar*     xmlSecOpenSSLGetDefaultTrustedCertsFolde
  * What version of the openssl API do we have? (also see configure.ac)
  *
  *******************************************************************/
-#if OPENSSL_VERSION_NUMBER == 0x20000000L && defined(LIBRESSL_VERSION_NUMBER)
+#if OPENSSL_VERSION_NUMBER == 0x20000000L && (defined(LIBRESSL_VERSION_NUMBER) && LIBRESSL_VERSION_NUMBER < 0x20700000L)
 /* Libressl decided to take over OpenSSL version 2.0.0, likely will create
  * issues down the road...
  */
 #define XMLSEC_OPENSSL_API_100      1
-#elif OPENSSL_VERSION_NUMBER >= 0x10100000L
+#elif (defined(LIBRESSL_VERSION_NUMBER) && LIBRESSL_VERSION_NUMBER >= 0x20700000L) || OPENSSL_VERSION_NUMBER >= 0x10100000L
 #define XMLSEC_OPENSSL_API_110      1
 #elif OPENSSL_VERSION_NUMBER >= 0x10000000L
 #define XMLSEC_OPENSSL_API_100      1

1f844f726ab880ce2eb6b4596510eed9e94d2bca
Bernard Spil <Sp1l@users.noreply.github.com>
2018-04-23 18:39:14 +0200

    Fix build with LibreSSL 2.7 (#167)
    * Fix build with LibreSSL 2.7

--- include/xmlsec/openssl/crypto.h
+++ include/xmlsec/openssl/crypto.h
@@ -43,11 +43,13 @@ XMLSEC_CRYPTO_EXPORT const xmlChar*     xmlSecOpenSSLGetDefaultTrustedCertsFolde
  * What version of the openssl API do we have? (also see configure.ac)
  *
  *******************************************************************/
-#if OPENSSL_VERSION_NUMBER == 0x20000000L && defined(LIBRESSL_VERSION_NUMBER)
-/* Libressl decided to take over OpenSSL version 2.0.0, likely will create
- * issues down the road...
- */
+#if defined(LIBRESSL_VERSION_NUMBER) && LIBRESSL_VERSION_NUMBER < 0x20700000L
+/* LibreSSL decided to take over OpenSSL version 2.0.0, likely will create
+ * issues down the road... */
 #define XMLSEC_OPENSSL_API_100      1
+#elif defined(LIBRESSL_VERSION_NUMBER) && LIBRESSL_VERSION_NUMBER >= 0x20700000L
+/* LibreSSL 2.7 implements (most of) OpenSSL 1.1 API */
+#define XMLSEC_OPENSSL_API_110      1
 #elif OPENSSL_VERSION_NUMBER >= 0x10100000L
 #define XMLSEC_OPENSSL_API_110      1
 #elif OPENSSL_VERSION_NUMBER >= 0x10000000L
--- src/openssl/openssl_compat.h
+++ src/openssl/openssl_compat.h
@@ -49,4 +49,19 @@
 
 #endif /* !defined(XMLSEC_OPENSSL_API_110) */
 
+/******************************************************************************
+ *
+ * LibreSSL 2.7 compatibility (implements most of OpenSSL 1.1 API)
+ *
+ *****************************************************************************/
+#if defined(LIBRESSL_VERSION_NUMBER) && defined(XMLSEC_OPENSSL_API_110)
+/* EVP_CIPHER_CTX stuff */
+#define EVP_CIPHER_CTX_encrypting(x)       ((x)->encrypt)
+
+/* X509 stuff */
+#define X509_STORE_CTX_get_by_subject      X509_STORE_get_by_subject
+#define X509_OBJECT_new()                  (calloc(1, sizeof(X509_OBJECT)))
+#define X509_OBJECT_free(x)                { X509_OBJECT_free_contents(x); free(x); }
+#endif /* defined(LIBRESSL_VERSION_NUMBER) && defined(XMLSEC_OPENSSL_API_110) */
+
 #endif /* __XMLSEC_OPENSSL_OPENSSL_COMPAT_H__ */

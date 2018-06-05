72a0a04283b45ec7a626759a323cc3f6517eba34
lsh123 <aleksey@aleksey.com>
2018-06-05 09:36:57 -0700

    Xmlsec 1.2.26 release (#182)
    * preparing 1.2.26 release
    
    * fix html
    
    * restore deleted file
    
    * remove docs/api/tmpl folder references
    
    * fix doc tools detection
    
    * update build script
    
    * fix html
    
    * fix news

--- scripts/build_release.sh
+++ scripts/build_release.sh
@@ -1,7 +1,12 @@
 #!/bin/sh 
+#
+# Usage: build_release.sh <version> [<release-candidate-tag>]
+#
+
 
 # config
 version=$1
+rc=$2
 cur_pwd=`pwd`
 today=`date +%F-%H-%M-%S`
 
@@ -10,11 +15,13 @@ rpm_root=/usr/src/redhat
 build_root="/tmp/xmlsec-build-area-$today"
 tar_file="xmlsec1-$version.tar.gz"
 sig_file="xmlsec1-$version.sig"
+rc_tar_file="xmlsec1-$version-$rc.tar.gz"
+rc_sig_file="xmlsec1-$version-$rc.sig"
 git_release_branch="xmlsec-$version-release"
 git_version_tag=`echo $version | sed 's/\./_/g'`
 
 if [ x"$version" = x ]; then
-    echo "Usage: $0 <version>"
+    echo "Usage: $0 <version> [<release-candidate-tag>]"
     exit 1
 fi
 
@@ -26,7 +33,10 @@ cd "$build_root"
 echo "============== Checking out the module '$git_url'"
 git clone $git_uri
 cd xmlsec
-#git checkout $git_release_branch
+if [ x"$rc" != x ]; then
+    echo "============== Switching to release branch '$git_release_branch' for RC build '$rc'"
+    git checkout $git_release_branch
+fi
 find . -name ".git" | xargs rm -r
 
 echo "============== Building xmlsec1-$version"
@@ -35,16 +45,26 @@ make tar-release
 # can't build rpm on ubuntu
 # make rpm-release
 
-echo "============== Signing $tar_file into $sig_file"
-gpg --output "$sig_file" --detach-sig "$tar_file"
-
-echo "============== Tagging the release $version in the github"
-echo "git tag -a "xmlsec-$git_version_tag" -m 'XMLSec release $version'"
-echo "git push --follow-tags"
-
-echo "============== Move files and cleanup"
-mv "$tar_file" "$sig_file" "$cur_pwd/"
+echo "============== Moving tar file"
+if [ x"$rc" = x ]; then
+     mv "$tar_file" "$cur_pwd/"
+else
+     mv "$tar_file" "$cur_pwd/$rc_tar_file"
+fi
 cd "$cur_pwd"
+
+echo "============== Signing tar file"
+if [ x"$rc" = x ]; then
+    gpg --output "$sig_file" --detach-sig "$tar_file"
+else
+    gpg --output "$rc_sig_file" --detach-sig "$rc_tar_file"
+fi
+
+if [ x"$rc" = x ]; then
+     echo "============== Tagging the release $version in the github"
+     echo "RUN MANUALLY: git tag -a "xmlsec-$git_version_tag" -m 'XMLSec release $version'"
+     echo "RUN MANUALLY: git push --follow-tags"
+fi
+
+echo "============== Cleanup"
 #rm -rf "$build_root"
-
-

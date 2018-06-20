42fae509f9c7b6fa78f1daf394bdb6db00aa9734
Aleksey Sanin <aleksey@aleksey.com>
2018-06-19 20:32:36 -0700

    fix output file

--- tests/testrun.sh
+++ tests/testrun.sh
@@ -407,7 +407,7 @@ execEncTest() {
         rm -f $tmpfile
         printf "    Decrypt existing document                            "
         echo "$VALGRIND $xmlsec_app decrypt $xmlsec_params $params1 $full_file.xml" >>  $curlogfile 
-        $VALGRIND $xmlsec_app decrypt $xmlsec_params $params1 $full_file.xml  2>> $curlogfile > $tmpfile
+        $VALGRIND $xmlsec_app decrypt $xmlsec_params $params1 --output $tmpfile $full_file.xml >> $curlogfile  2>> $curlogfile
         res=$?
         echo "=== TEST RESULT: $res; expected: $expected_res" >> $curlogfile
         if [ $res = 0 -a "$expected_res" = "$res_success" ]; then

321e62add243cf8f024d6278da4c5ff030bae3b9
Alon Bar-Lev <alon.barlev@gmail.com>
2019-04-01 01:28:18 +0300

    gnutls: allow SHA-1 signed certificate when not in strict checks (#250) (#251)
    This is required for gnutls-3.6.x.
    
    Allow tests to use no strict checks until all certificates will be converted
    to stronger signature than SHA-1.
    
    Signed-off-by: Alon Bar-Lev <alon.barlev@gmail.com>

--- src/gnutls/x509vfy.c
+++ src/gnutls/x509vfy.c
@@ -295,6 +295,9 @@ xmlSecGnuTLSX509StoreVerify(xmlSecKeyDataStorePtr store,
     if((keyInfoCtx->flags & XMLSEC_KEYINFO_FLAGS_X509DATA_SKIP_STRICT_CHECKS) != 0) {
         flags |= GNUTLS_VERIFY_ALLOW_SIGN_RSA_MD2;
         flags |= GNUTLS_VERIFY_ALLOW_SIGN_RSA_MD5;
+#if GNUTLS_VERSION_NUMBER >= 0x030600
+        flags |= GNUTLS_VERIFY_ALLOW_SIGN_WITH_SHA1;
+#endif
     }
 
     /* We are going to build all possible cert chains and try to verify them */

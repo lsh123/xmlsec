466ec74e060864c32f23f57cb6abf34a8fa2586f
snargit <dave.bailey@outlook.com>
2022-05-06 13:53:14 +0100

    Add missing includes

--- src/openssl/hmac.c
+++ src/openssl/hmac.c
@@ -31,7 +31,9 @@
 #include <stdio.h>
 #include <string.h>
 
+#include <openssl/core_names.h>
 #include <openssl/hmac.h>
+#include <openssl/param_build.h>
 
 #include <xmlsec/xmlsec.h>
 #include <xmlsec/xmltree.h>

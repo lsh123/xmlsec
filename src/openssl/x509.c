f1efe6f0789072cde848ab9b7ffc6cbb629537bb
Aleksey Sanin <aleksey@aleksey.com>
2022-06-22 20:38:14 -0400

    minor tweaks

--- src/openssl/x509.c
+++ src/openssl/x509.c
@@ -56,16 +56,6 @@
 
 #include "openssl_compat.h"
 
-
-/* The ASN1_TIME_check() function was changed from ASN1_TIME * to
- * const ASN1_TIME * in 1.1.0. To avoid compiler warnings, we use this hack.
- */
-#if !(defined(XMLSEC_OPENSSL_API_110) || defined(XMLSEC_OPENSSL_API_300)) || defined(OPENSSL_IS_BORINGSSL)
-typedef ASN1_TIME XMLSEC_CONST_ASN1_TIME;
-#else  /* !(defined(XMLSEC_OPENSSL_API_110) || defined(XMLSEC_OPENSSL_API_300)) || defined(OPENSSL_IS_BORINGSSL) */
-typedef const ASN1_TIME XMLSEC_CONST_ASN1_TIME;
-#endif /* !(defined(XMLSEC_OPENSSL_API_110) || defined(XMLSEC_OPENSSL_API_300)) || defined(OPENSSL_IS_BORINGSSL) */
-
 /*************************************************************************
  *
  * X509 utility functions

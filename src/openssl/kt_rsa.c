82ed61852ae146e2d8011ea2dcf7c3468205d9f6
Aleksey Sanin <aleksey@aleksey.com>
2022-11-23 18:39:43 -0500

    update docs

--- src/openssl/kt_rsa.c
+++ src/openssl/kt_rsa.c
@@ -1292,8 +1292,7 @@ xmlSecOpenSSLRsaOaepProcess(xmlSecTransformPtr transform) {
 
     ret = xmlSecBufferRemoveHead(in, inSize);
     if(ret < 0) {
-        xmlSecInternalError2("xmlSecBufferRemoveHead",<li>Added support for <a href="https://www.w3.org/TR/xmlenc-core1/#sec-RSA-OAEP">MGF1 algorithms</a> 
-			to xmlsec-openssl and xmlsec-mscng (see <a href="xmlenc.html">Interoperability report</a> for more details).</li>
+        xmlSecInternalError2("xmlSecBufferRemoveHead",
             xmlSecTransformGetName(transform),
             "size=" XMLSEC_SIZE_FMT, inSize);
         return(-1);

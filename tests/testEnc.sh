834a037ec1a673445604f346294d835a4444e0ac
Aleksey Sanin <aleksey@aleksey.com>
2022-11-23 14:12:25 -0500

    fix tests

--- tests/testEnc.sh
+++ tests/testEnc.sh
@@ -393,7 +393,7 @@ execEncTest $res_success \
 execEncTest $res_success \
     "" \
     "01-phaos-xmlenc-3/enc-text-aes256-kt-rsa_oaep_sha1" \
-    "aes256-cbc rsa-oaep-mgf1p sha1" \
+    "aes256-cbc rsa-oaep-mgf1p sha1  sha1" \
     "$priv_key_option $topfolder/01-phaos-xmlenc-3/rsa-priv-key.$priv_key_format --pwd secret" \
     "--session-key aes-256 --keys-file $topfolder/01-phaos-xmlenc-3/keys.xml --enabled-key-data key-name --xml-data $topfolder/01-phaos-xmlenc-3/enc-text-aes256-kt-rsa_oaep_sha1.data --node-name http://example.org/paymentv2:CreditCard"  \
     "$priv_key_option $topfolder/01-phaos-xmlenc-3/rsa-priv-key.$priv_key_format --pwd secret"
